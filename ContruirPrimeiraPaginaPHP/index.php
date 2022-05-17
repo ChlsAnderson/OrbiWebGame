17e4c0e0222abf151c4ee76925e2c16d89634711
Charles Mesquita <charles_mesquita@kawasakibrasil.com>
2022-05-17 14:10:55 -0300

    Primeiro commit com atalho do backend para subir o php e arquivo index php

---
+++ ContruirPrimeiraPaginaPHP/index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>Meu primeiro site em PHP! Nice!</title>
+	</head>
+	<body>
+		<?php 
+			for ($i = 0 ; $i < 10 ; $i++){
+				print("Linha Numero ".$i."<br/>");
+			}
+		?>
+	</body>
+</html>
